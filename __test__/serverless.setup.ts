283874c827fc08791d2bab5b4d28a9228b23fdf5
Arthur Reis <arthurreis074@gmail.com>
2024-01-27 04:42:42 -0300

    chore: :wrench: charge jest dev server host to localhost

--- __test__/serverless.setup.ts
+++ __test__/serverless.setup.ts
@@ -3,7 +3,8 @@ import { setup } from "jest-dev-server";
 export default async function globalSetup() {
 	globalThis.servers = await setup({
 		command: "sls offline start --httpPort 4000",
-		launchTimeout: 50000,
+		launchTimeout: 10000,
+		host: "localhost",
 		port: 4000,
 	});
 }
